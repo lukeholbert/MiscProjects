36ad90debe64701e5d9fa5d850ce2f7435b0fa55
lukeholbert <luke.holbert@email.wsu.edu>
2016-09-04 18:09:25 -0700

    Added 122 HW

---
+++ Homework Assignments/Tetris/Tetris/Tetris.cpp
@@ -0,0 +1,13 @@
+#include "Game.h"
+
+using namespace System;
+using namespace System ::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Tetris::Game form;
+	Application::Run(%form);
+}
